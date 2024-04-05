d4f5ab801ac867a9a5639beed4908e8a5e4ee26e
Lathansingharath <singharath@hotmail.com>
2024-04-05 07:05:56 -0500

    done

---
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/example/countingLetters.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class countingLetters {
+
+    public static void  main(String[] args) {
+
+        String countString = "If the product of two terms is zero then " +
+                "common sense says at least one of the two terms has to be " +
+                "zero to start with. So if you move all the terms over to one " +
+                "side, you can put the quadratics into a form that can be factored " +
+                "allowing that side of the equation to equal zero. Once you’ve done that, " +
+                "it’s pretty straightforward from there.";
+
+        //System.out.println(countString);//testing
+        char[] stringChar = countString.toCharArray();
+       // System.out.println(stringChar[0]);//testing
+        HashMap<Character,Integer> countingCh = new HashMap<>();
+
+
+
+
+
+    }
+}
