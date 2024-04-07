483351ca4d8538afd07393d625ae8df9b0ec2fbe
Lathansingharath <singharath@hotmail.com>
2024-04-07 04:33:47 -0500

    done again

--- control-flow-and-collections/studio/counting-characters/src/main/java/org/example/countingLetters.java
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/example/countingLetters.java
@@ -1,6 +1,7 @@
 package org.example;
-
 import java.util.HashMap;
+import java.util.Map;
+import  java.util.Scanner;
 
 public class countingLetters {
 
@@ -12,15 +13,57 @@ public class countingLetters {
                 "side, you can put the quadratics into a form that can be factored " +
                 "allowing that side of the equation to equal zero. Once you’ve done that, " +
                 "it’s pretty straightforward from there.";
+//////////////////////////////////The codes between the comment lines are for user input.
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a sentence");
+        String stringInput = input.nextLine();
+        char[] charInput = stringInput.toCharArray();
+        HashMap<Character,Integer> inputCountingCh = new HashMap<>();
 
-        //System.out.println(countString);//testing
+
+        for (char inputLetter : charInput){
+            int counter=0;
+            if (!inputCountingCh.containsKey(inputLetter)) {
+                for (int i = 0;i < charInput.length; i++){
+                    if (inputLetter == charInput[i]) {
+                        counter += 1;
+                    }
+                }
+                inputCountingCh.put(inputLetter,counter);
+            }
+        }
+
+        for (Map.Entry<Character,Integer> userLetterCount : inputCountingCh.entrySet()) {
+            if(!userLetterCount.getKey().equals(' ')) {
+                System.out.println(userLetterCount.getKey()+ ": " +userLetterCount.getValue());
+            }
+        }
+
+        System.out.println("\n");
+
+
+
+////////////////////////////////////////////////////////////////
         char[] stringChar = countString.toCharArray();
-       // System.out.println(stringChar[0]);//testing
         HashMap<Character,Integer> countingCh = new HashMap<>();
 
-
-
-
+        for (char letter : stringChar){
+            int counter=0;
+            if (!countingCh.containsKey(letter)) {
+                for (int i = 0;i < stringChar.length; i++){
+                    if (letter == stringChar[i]) {
+                        counter += 1;
+                    }
+                }
+                countingCh.put(letter,counter);
+            }
+        }
+        System.out.println(countString);
+        for (Map.Entry<Character,Integer> letterCount : countingCh.entrySet()) {
+            if(!letterCount.getKey().equals(' ')) {
+                System.out.println(letterCount.getKey()+ ": " +letterCount.getValue());
+            }
+        }
 
     }
 }
